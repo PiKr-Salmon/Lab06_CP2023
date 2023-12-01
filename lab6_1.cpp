c984bb6a6d904d0d6ea99ca5b9eaad5c73c56a5e
PiKr-Salmon <piyawat_kr@cmu.ac.th>
2023-12-01 15:38:36 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+    int ct = 0;
+    int even = 0, odd = 0; 
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> ct;
+    while(ct != 0){
+        if(ct%2 == 0){
+            even++;
+        }else{
+            odd++;
+        }
+        cout << "Enter an integer: ";
+        cin >> ct;
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = "<< odd << endl;
     return 0;
 }
